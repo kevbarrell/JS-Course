e00c806e8c08c4cae11e5514670b6b7f8379b24a
kevbarrell <kevbarrell@gmail.com>
2025-02-19 17:35:23 -0800

    initial commit

---
+++ assignment01/js/script.js
@@ -0,0 +1,177 @@
+// STEP 1
+
+//let someMonth
+//function theMonth()
+//let currentMonth
+//let summerMonth
+//let myLibraryFunction
+
+// STEP 2
+
+//let someMonth = 1;
+//let currentMonth = "January";
+//let summerMonth = false;
+//let myLibraryFunction = null;
+
+// STEP 3
+
+//let currentMonth = "January";
+//let currentDay = "1";
+//let currentDate = currentMonth + currentDay;
+
+//let a = 1;
+//let b = 7;
+//let sum = a + b;
+
+// STEP 4
+
+//Camel Case
+//let firstName;
+//let lastName;
+//let address;
+//let city;
+//let state;
+//let zipCode;
+//let yourAge;
+//let referralSource;
+//let mayWeContactYou;
+
+//Hungarian Notation
+//let strFirstName;
+//let strLastName;
+//let strAddress;
+//let strCity;
+//let strState;
+//let strZipCode;
+//let intYourAge;
+//let strReferralSource;
+//let boolMayWeContactYou;
+
+// STEP 5
+
+//let addressInfo = {
+//    strCity: "San Diego",
+//    strState: "California",
+//    strZipCode: "92101"
+//};
+
+//let strCity = "San Diego";
+//let strState = "California";
+//let strZipCode = "92101";
+
+//let strCity, strState, strZipCode;
+//strCity = "San Diego";
+//strState = "California";
+//strZipCode = "92101";
+
+// STEP 6
+
+//strState = "California ";
+//intZipCode = 92101;
+//let result = strState + intZipCode;
+//console.log(result); //Result: California 92101
+
+// STEP 7
+
+//let firstVariable = true + " is the first variable";
+//console.log(firstVariable); //Result: true is the first variable
+
+//let secondVariable = true + 0;
+//console.log(secondVariable); //Result: 1
+
+// STEP 8
+
+//No it is not valid because of the apostrophe in the word I'm. I would correct it like so:
+//let someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\""
+//console.log(someString)
+
+// STEP 9
+
+//let firstVariable = null;
+//console.log(firstVariable); //Result: null
+
+//let secondVariable;
+//console.log(secondVariable); //Result: undefined
+
+// STEP 10
+
+//console.log(typeof "Hello"); //string
+//console.log(typeof 1); //number
+//console.log(typeof true); //boolean
+//console.log(typeof null); //object
+//console.log(typeof undefined); //undefined
+
+// STEP 11
+
+//alert("Hello" + " Kev Barrell" + ", welcome to the JavaScript class!")
+
+// STEP 12
+
+//let studentName = "Kev Barrell"
+//alert("Hello " + studentName + ", welcome to the JavaScript class!")
+
+// STEP 13
+
+//let studentName = " Kev Barrell"
+//let className = "JavaScript"
+//alert("Hello" + studentName + ", welcome to the " + className + " class!")
+
+// STEP 14
+
+//let studentName = " Kev Barrell"
+//let className = "JavaScript"
+//alert("Hello" + studentName + ". \nWelcome to the " + className + " class!")
+
+// STEP 15
+
+//let studentName = prompt("What is your name?");
+//let className = "JavaScript";
+//alert("Hello " + studentName + ". \nWelcome to the " + className + " class!");
+
+
+// STEP 16
+
+//let studentName = prompt("What is your name?");
+//let className = prompt("What class are you taking?");
+//alert("Hello " + studentName + ". \nWelcome to the " + className + " class!");
+
+// STEP 17
+
+//let x = 10;
+//let y = 20;
+//let sum = x + y;
+//console.log(sum);
+
+// STEP 18
+
+//let x = 20;
+//x += 20;
+//console.log(x); //Result: 40
+
+
+// STEP 19
+
+//let x = 20;
+//x *= 5;
+//console.log(x); //Result: 100
+
+// STEP 20
+
+//let x = 20 % 3;
+//x /= 1;
+//console.log(x); //Result: 2
+
+// STEP 21
+
+//let a = 10;
+//let b = 20;
+//let c = 30;
+//let result = (a < b) && (b < c) || (c === 30 && a !== b);
+//console.log(result); //Result: true
+
+// STEP 22
+//let x = 15;
+//let y = 25;
+//let z = 35;
+//let result = (x > y) || !(z <= 35) && (y === x);
+//console.log(result); //Result: false
